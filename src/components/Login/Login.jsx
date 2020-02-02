7503ea4335b608cd71e3d2e96c467c70c1f62217
Emil <emilpet93@gmail.com>
2020-02-02 17:05:33 +0100

    Setup with some basic project structure

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.scss";
+
+const Login = () => {
+  return (
+    <main className="login">
+      <h1>onBoarding</h1>
+    </main>
+  );
+};
+
+export default Login;
