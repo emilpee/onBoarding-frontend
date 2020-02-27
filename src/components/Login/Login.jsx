7db774bf31f0e2b1da17e6978088c0fbc757e0ed
Emil <emilpet93@gmail.com>
2020-02-27 16:59:05 +0100

    Started implementing oauth

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { CLIENT_ID } from "../../boardgameatlas.config";
 import "./styles.scss";
 
 const Login = props => {
@@ -7,7 +9,28 @@ const Login = props => {
   const [password, setPassword] = useState("");
   const [user, setUser] = useState({ email, password });
 
+  const query = window.location.search.substring(1);
+  const token = query.split("access_token=")[1];
+
   useEffect(() => {}, []);
+
+  const handleOauth = () => {
+    const config = {
+      method: "get",
+      url: `https://cors-anywhere.herokuapp.com/https://www.boardgameatlas.com/oauth/authorize?response_type=code&client_id=${CLIENT_ID}&redirect_uri=localhost:3000/dashboard`,
+      headers: {
+        Authorization: "token " + token
+      }
+    };
+
+    axios(config)
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        throw Error(err);
+      });
+  };
 
   const handleFormSubmit = e => {
     e.preventDefault();
@@ -15,7 +38,7 @@ const Login = props => {
       email,
       password
     });
-    history.push("/dashboard");
+    // history.push("/dashboard");
   };
 
   const handleInputChange = event => {
@@ -30,7 +53,7 @@ const Login = props => {
     <main className="login container">
       <h1>onBoarding</h1>
       <form onSubmit={handleFormSubmit} className="ui form">
-        <h2>Sign in</h2>
+        <h3>Sign in</h3>
         <div className="input field">
           <label htmlFor="email">Email</label>
           <input onChange={handleInputChange} type="email" id="email"></input>
@@ -48,6 +71,10 @@ const Login = props => {
             Login
           </button>
         </div>
+
+        <button onClick={handleOauth} className="ui button" type="submit">
+          Oauth
+        </button>
       </form>
     </main>
   );
