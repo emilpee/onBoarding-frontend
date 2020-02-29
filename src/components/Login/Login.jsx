87c02161b699ac9afe2adbc73b66362826ca2695
Emil <emilpet93@gmail.com>
2020-02-29 16:56:11 +0100

    Started to implement oauth with board game atlas API

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,81 +1,33 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import { CLIENT_ID } from "../../boardgameatlas.config";
 import "./styles.scss";
 
 const Login = props => {
-  const { history } = props;
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [user, setUser] = useState({ email, password });
-
-  const query = window.location.search.substring(1);
-  const token = query.split("access_token=")[1];
-
   useEffect(() => {}, []);
 
-  const handleOauth = () => {
-    const config = {
-      method: "get",
-      url: `https://cors-anywhere.herokuapp.com/https://www.boardgameatlas.com/oauth/authorize?response_type=code&client_id=${CLIENT_ID}&redirect_uri=localhost:3000/dashboard`,
-      headers: {
-        Authorization: "token " + token
-      }
-    };
-
-    axios(config)
-      .then(res => {
-        console.log(res);
-      })
-      .catch(err => {
-        throw Error(err);
-      });
-  };
-
-  const handleFormSubmit = e => {
-    e.preventDefault();
-    setUser({
-      email,
-      password
-    });
-    // history.push("/dashboard");
-  };
-
-  const handleInputChange = event => {
-    if (event.target.type === "email") {
-      setEmail(event.target.value);
-    } else {
-      setPassword(event.target.value);
+  const handleConnectClick = () => {
+    function generateUrlWithState() {
+      // TODO - kryptera
+      const state = Math.random()
+        .toString(36)
+        .substring(7);
+      return `https://www.boardgameatlas.com/oauth/authorize?response_type=code&client_id=${CLIENT_ID}&state=${state}&redirect_uri=http://localhost:8080/oauth/redirect`;
     }
+
+    window.location.href = generateUrlWithState();
   };
 
   return (
     <main className="login container">
       <h1>onBoarding</h1>
-      <form onSubmit={handleFormSubmit} className="ui form">
-        <h3>Sign in</h3>
-        <div className="input field">
-          <label htmlFor="email">Email</label>
-          <input onChange={handleInputChange} type="email" id="email"></input>
-        </div>
-        <div className="input field">
-          <label htmlFor="password">Password</label>
-          <input
-            onChange={handleInputChange}
-            type="password"
-            id="password"
-          ></input>
-        </div>
-        <div className="input field">
-          <button className="ui button" type="submit">
-            Login
-          </button>
-        </div>
+      <p>
+        Welcome! In order to use this application, you must connect to Board
+        Game Atlas. Click the button to begin authenticating.
+      </p>
 
-        <button onClick={handleOauth} className="ui button" type="submit">
-          Oauth
-        </button>
-      </form>
+      <a onClick={handleConnectClick} className="ui button">
+        Connect to BGA
+      </a>
     </main>
   );
 };
