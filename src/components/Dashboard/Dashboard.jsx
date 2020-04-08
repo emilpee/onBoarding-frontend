8c14e93571b8788bf6092464daa53939a9106052
Emil <emilpet93@gmail.com>
2020-04-08 20:54:30 +0200

    Received response from backend/api

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { CLIENT_ID } from "../../boardgameatlas.config";
 
 const Dashboard = () => {
+  const [gamesData, setgamesData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://www.boardgameatlas.com/api/search?order_by=popularity&client_id=${CLIENT_ID}`
+      )
+      .then(response => {
+        setgamesData(response.data.games);
+      });
+  }, []);
+
   return (
-    <div className="container">
-      <div>DASHBOARD!</div>
+    <div>
+      <h1>DASHBOARD!</h1>
+      <ul>
+        {gamesData.length > 0 &&
+          gamesData.map(game => {
+            return <li key={game.id}>{game.name}</li>;
+          })}
+      </ul>
     </div>
   );
 };
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,33 +1,29 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { CLIENT_ID } from "../../boardgameatlas.config";
+import { Button } from "react-bootstrap";
 import "./styles.scss";
 
-const Login = props => {
-  useEffect(() => {}, []);
-
+const Login = () => {
   const handleConnectClick = () => {
     function generateUrlWithState() {
-      // TODO - kryptera
       const state = Math.random()
         .toString(36)
         .substring(7);
-      return `https://www.boardgameatlas.com/oauth/authorize?response_type=code&client_id=${CLIENT_ID}&state=${state}&redirect_uri=http://localhost:8080/oauth/redirect`;
+      return `https://www.boardgameatlas.com/oauth/authorize?response_type=code&client_id=${CLIENT_ID}&state=${state}&redirect_uri=http://localhost:8080/oauth`;
     }
 
     window.location.href = generateUrlWithState();
   };
 
   return (
-    <main className="login container">
+    <main className="login">
       <h1>onBoarding</h1>
       <p>
         Welcome! In order to use this application, you must connect to Board
-        Game Atlas. Click the button to begin authenticating.
+        Game Atlas. Click the button to authenticate.
       </p>
 
-      <a onClick={handleConnectClick} className="ui button">
-        Connect to BGA
-      </a>
+      <Button onClick={handleConnectClick}>Connect to BGA</Button>
     </main>
   );
 };
