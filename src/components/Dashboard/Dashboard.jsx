a85b9be7aea20f0f1b06fa0e841dc0c37037ecdb
Emil <emilpet93@gmail.com>
2020-05-01 17:59:46 +0200

    Got user data response from api

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -4,23 +4,38 @@ import { CLIENT_ID } from "../../boardgameatlas.config";
 
 const Dashboard = () => {
   const [gamesData, setgamesData] = useState([]);
+  const [user, setUser] = useState(null);
+  const query = window.location.search.substring(1);
+  const token = query.split("access_token=")[1];
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://cors-anywhere.herokuapp.com/https://www.boardgameatlas.com/api/user/data?client_id=${CLIENT_ID}`,
+        { headers: { Authorization: `Bearer ${token}` } }
+      )
+      .then(({ data }) => {
+        setUser(data.user);
+      });
+  });
 
   useEffect(() => {
     axios
       .get(
         `https://www.boardgameatlas.com/api/search?order_by=popularity&client_id=${CLIENT_ID}`
       )
-      .then(response => {
-        setgamesData(response.data.games);
+      .then(({ data }) => {
+        setgamesData(data.games);
       });
   }, []);
 
   return (
     <div>
       <h1>DASHBOARD!</h1>
+      {user !== null && <h3>Welcome {user.username}!</h3>}
       <ul>
         {gamesData.length > 0 &&
-          gamesData.map(game => {
+          gamesData.map((game) => {
             return <li key={game.id}>{game.name}</li>;
           })}
       </ul>
