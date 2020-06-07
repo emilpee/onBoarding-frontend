8399cb935a8646c1d49bdc1804dbc8f562ae760b
Emil Petersson <emilpet93@gmail.com>
2020-06-07 14:58:56 +0200

    Sets user when arriving at dashboard

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,31 +1,38 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { CLIENT_ID } from "../../boardgameatlas.config";
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { CLIENT_ID } from '../../boardgameatlas.config'
 
 const Dashboard = () => {
-  const [gamesData, setgamesData] = useState([]);
+    const [gamesData, setgamesData] = useState([])
+    const [user, setUser] = useState(null)
 
-  useEffect(() => {
-    axios
-      .get(
-        `https://www.boardgameatlas.com/api/search?order_by=popularity&client_id=${CLIENT_ID}`
-      )
-      .then(response => {
-        setgamesData(response.data.games);
-      });
-  }, []);
+    useEffect(() => {
+        axios.get('http://localhost:8080/user').then((response) => {
+            const [user] = response.data
+            setUser(user)
+        })
+    }, [])
 
-  return (
-    <div>
-      <h1>DASHBOARD!</h1>
-      <ul>
-        {gamesData.length > 0 &&
-          gamesData.map(game => {
-            return <li key={game.id}>{game.name}</li>;
-          })}
-      </ul>
-    </div>
-  );
-};
+    useEffect(() => {
+        axios
+            .get(`https://www.boardgameatlas.com/api/search?order_by=popularity&client_id=${CLIENT_ID}`)
+            .then((response) => {
+                setgamesData(response.data.games)
+            })
+    }, [])
 
-export default Dashboard;
+    return (
+        <div>
+            <h1>DASHBOARD!</h1>
+            {user !== null && <h3>Hej {user.username}!</h3>}
+            <ul>
+                {gamesData.length > 0 &&
+                    gamesData.map((game) => {
+                        return <li key={game.id}>{game.name}</li>
+                    })}
+            </ul>
+        </div>
+    )
+}
+
+export default Dashboard
