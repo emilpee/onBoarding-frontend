7bafc28f6388c5c8325779a28be36f0fba39c8ce
Emil <emilpet93@gmail.com>
2020-09-09 21:42:24 +0200

    Create a gamelist page

---
+++ src/views/Games/Games.tsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState, FunctionComponent } from "react";
+import axios from "axios";
+import { CLIENT_ID } from "../../boardgameatlas.config";
+import { GameObject } from "../../interfaces";
+import { UserContext } from "../../context/userContext";
+import { Container, Spinner } from "react-bootstrap";
+
+const Games: FunctionComponent = () => {
+  const [gamesData, setgamesData] = useState<GameObject[]>([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.boardgameatlas.com/api/search?order_by=popularity&client_id=${CLIENT_ID}`
+      )
+      .then(({ data }) => {
+        setgamesData(data.games);
+      });
+  }, []);
+
+  return (
+    <UserContext.Provider value={null}>
+      <Container className="container" fluid>
+        <h1 className="display-4">Browse Games</h1>
+        <ul>
+          {gamesData.length > 0 ? (
+            gamesData.map((game: GameObject) => {
+              return <li key={game.id}>{game.name}</li>;
+            })
+          ) : (
+            <Spinner animation="border" color="primary" />
+          )}
+        </ul>
+      </Container>
+    </UserContext.Provider>
+  );
+};
+
+export default Games;
