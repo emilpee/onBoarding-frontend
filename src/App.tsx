99d9adc891e43a56c3ac632917647abf0b24a729
Emil <emilpet93@gmail.com>
2020-09-12 19:47:06 +0200

    Store user data in localStorage instead of state

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import React, { FunctionComponent } from "react";
+import Footer from "components/Footer";
+import Header from "components/Header";
+import React, { FunctionComponent, useEffect } from "react";
 import {
   BrowserRouter as Router,
   Route,
@@ -6,22 +8,42 @@ import {
   Redirect,
 } from "react-router-dom";
 import "./scss/index.scss";
-import { Login, Dashboard, Games } from "./views";
+import { Login, Dashboard, GameList } from "./views";
+import axios from "axios";
+import { UserContext } from "context/userContext";
 
 interface AppProps extends RouteComponentProps {
   handleLogin: () => void;
 }
 
 const App: FunctionComponent<AppProps> = () => {
-  const ProtectedRoute = ({ component: Component, user, ...rest }) => {
+  const user = JSON.parse(localStorage.getItem("user"));
+
+  useEffect(() => {
+    const query = window.location.search.substring(1);
+    const token = query.split("access_token=")[1];
+    const id = query.split("user=")[1];
+
+    if (token) {
+      axios
+        .get(`http://localhost:8080/users/${id}`)
+        .then(({ data }) => {
+          localStorage.setItem("user", JSON.stringify(data));
+        })
+        .catch((err) => {
+          throw new Error(err);
+        });
+    }
+  });
+
+  const ProtectedRoute = ({ component: Component, ...rest }) => {
     return (
       <Route
         {...rest}
         render={(props) =>
-          sessionStorage.getItem("loggedIn") === "true" ? (
+          sessionStorage.getItem("loggedIn") === JSON.stringify(true) ? (
             <Component {...rest} {...props} />
           ) : (
-            // TODO - remove redirect if signed in
             <Redirect to="/login" />
           )
         }
@@ -36,21 +58,25 @@ const App: FunctionComponent<AppProps> = () => {
 
   return (
     <>
-      <Router>
-        <Route path="/" exact render={() => <Redirect to="/login" />} />
-        <Route
-          exact
-          path="/login"
-          render={(props) => <Login handleLogin={handleLogin} {...props} />}
-        />
-        <Route exact path="/games" component={Games} />
-        <ProtectedRoute
-          exact
-          user={sessionStorage.getItem("loggedIn")}
-          path="/dashboard"
-          component={Dashboard}
-        />
-      </Router>
+      {sessionStorage.getItem("loggedIn") === JSON.stringify(true) ? (
+        <Router>
+          <UserContext.Provider value={user}>
+            <Header />
+            <ProtectedRoute exact path="/games" component={GameList} />
+            <ProtectedRoute exact path="/dashboard" component={Dashboard} />
+            <Footer />
+          </UserContext.Provider>
+        </Router>
+      ) : (
+        <Router>
+          <Route path="/" exact render={() => <Redirect to="/login" />} />
+          <Route
+            exact
+            path="/login"
+            render={(props) => <Login handleLogin={handleLogin} {...props} />}
+          />
+        </Router>
+      )}
     </>
   );
 };
--- src/context/userContext.ts
+++ src/context/userContext.ts
@@ -1,12 +1,4 @@
 import { User } from "interfaces";
 import React from "react";
 
-const UserContext = React.createContext<User>({
-  id: "",
-  username: "",
-  country: "",
-  age: null,
-});
-
-export const UserProvider = UserContext.Provider;
-export default UserContext;
+export const UserContext = React.createContext<User>(null);
