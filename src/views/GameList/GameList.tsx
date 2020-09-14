053d823cb8e0e006da97d90e5d8244e52b617879
Emil <emilpet93@gmail.com>
2020-09-14 20:58:53 +0200

    Game cards are shown in the GameList view

--- src/views/GameList/GameList.tsx
+++ src/views/GameList/GameList.tsx
@@ -1,11 +1,12 @@
-import React, { useEffect, useState, FunctionComponent } from "react";
-import axios from "axios";
-import { CLIENT_ID } from "../../boardgameatlas.config";
-import { GameObject } from "../../interfaces";
-import { Container, Spinner } from "react-bootstrap";
+import React, { useEffect, useState, FunctionComponent } from 'react'
+import axios from 'axios'
+import { CLIENT_ID } from '../../boardgameatlas.config'
+import { GameObject } from '../../interfaces'
+import { CardColumns, CardDeck, Container, Spinner } from 'react-bootstrap'
+import { GameCard } from '../../components'
 
 const Games: FunctionComponent = () => {
-  const [gamesData, setgamesData] = useState<GameObject[]>([]);
+  const [gamesData, setgamesData] = useState<GameObject[]>([])
 
   useEffect(() => {
     axios
@@ -13,24 +14,24 @@ const Games: FunctionComponent = () => {
         `https://api.boardgameatlas.com/api/search?order_by=popularity&client_id=${CLIENT_ID}`
       )
       .then(({ data }) => {
-        setgamesData(data.games);
-      });
-  }, []);
+        setgamesData(data.games)
+      })
+  }, [])
 
   return (
     <Container className="container" fluid>
       <h1 className="display-4">Browse Games</h1>
-      <ul>
+      <CardColumns>
         {gamesData.length > 0 ? (
           gamesData.map((game: GameObject) => {
-            return <li key={game.id}>{game.name}</li>;
+            return <GameCard game={game} key={game.id} />
           })
         ) : (
           <Spinner animation="border" color="primary" />
         )}
-      </ul>
+      </CardColumns>
     </Container>
-  );
-};
+  )
+}
 
-export default Games;
+export default Games
